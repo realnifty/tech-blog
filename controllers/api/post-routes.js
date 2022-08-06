0d2be476ac3923104f9918bf5a2c804704564765
gordo <rogelioalbertopedraza@gmail.com>
2022-08-06 14:09:23 -0500

    replaced post URL references with text

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -9,7 +9,7 @@ router.get('/', (req, res) => {
   Post.findAll({
     attributes: [
       'id',
-      'post_url',
+      'post_text',
       'title',
       'created_at'
     ],
@@ -42,7 +42,7 @@ router.get('/:id', (req, res) => {
     },
     attributes: [
       'id',
-      'post_url',
+      'post_text',
       'title',
       'created_at'
     ],
